a087b3f334d78b2063f493e6fa20e4dc5e270073
solvedDev <solvedDev@gmail.com>
2020-12-06 16:00:47 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,10 +4,3 @@ import './index.css'
 
 const app = createApp(App)
 app.mount('#app')
-
-if ('serviceWorker' in navigator) {
-	// Use the window load event to keep the page load performant
-	window.addEventListener('load', () => {
-		navigator.serviceWorker.register('/service-worker.js')
-	})
-}
